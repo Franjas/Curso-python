c9685fa642fd83736cfe9583ff5d9ae6d5cc858e
Fran <franjas3@gmail.com>
2022-06-10 20:47:07 -0400

    Ejercicio5

---
+++ ejercicio5.py
@@ -0,0 +1,19 @@
+Nombre = 'Ronald'
+print(Nombre)
+Apellidos = 'Fernandez Gonzalez'
+print(Apellidos)
+Edad = 37
+print(Edad)
+Email = 'ronaldjs@gmail.com'
+print(Email)
+Teléfono = 4128354832
+print(Teléfono)
+Casado = False
+print(Casado)
+Hijos = None
+print(Hijos)
+Amigos = ['Victor', 'Jean', 'José', 'Luis', 'Andres', 'Ronald', 'Xavier',]
+print(Amigos)
+Diccionario = {'clave1': 'TLOTR', 'clave2': 'El Hobby', 'clave3': 'Mr Robot', 'clave4': 'Rescatando al soldado Ryan', 'clave5': 'El libro de los secretos'}
+PelículasVistas = Diccionario
+print(PelículasVistas)
