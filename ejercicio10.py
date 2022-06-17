5b8e95fc019deded00eff15bf97a54972c374e0f
Fran <franjas3@gmail.com>
2022-06-17 16:23:55 -0400

    Ejercicio10

---
+++ ejercicio10.py
@@ -0,0 +1,12 @@
+#3Escribe una función que calcule el área de un triángulo, recibiendo la altura y la base como parámetros y otra función que calcule el área de un círculo recibiendo el radio del mismo.
+
+def area_triángulo(base=6, altura=10):
+    return(base * altura) / 2
+
+def area_circulo(r=6, pi=3.1415):
+    return pi * (r * r)
+    
+
+resultado1 = area_triángulo(base=6, altura=10)
+resultado2 = area_circulo(r=6, pi=3.1415)
+print('El area del Triángulo es:',resultado1,'cm y el area del Circulo es:', resultado2,'metros^')
